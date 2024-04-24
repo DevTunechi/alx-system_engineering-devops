82e22e643e28d3dcd706458135b61e905e113b55
CodeReflexGame <codereflexgame@gmail.com>
2024-04-24 08:36:23 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# installs the package puppet-lint
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# Class: my_module::flask_installation
+
+  package { 'flask':
+    ensure   => '2.1.0',   # Ensure version 2.1.0 is installed
+    provider => 'pip3',    # Use pip3 provider to install Flask
+  }
+
+  package { 'werkzeug':
+    ensure   => '2.1.1',   # Ensure version 2.1.1 is installed
+    provider => 'pip3',    # Use pip3 provider to install Werkzeug
+  }
